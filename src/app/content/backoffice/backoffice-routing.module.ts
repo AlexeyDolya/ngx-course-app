1c881cbb67d8bef076dc4de065c47140726c36f4
sfs-inep <igornepipenko@gmail.com>
2019-04-16 18:54:13 +0300

    Feature: add events and charts pages [ref:no-ref]

--- src/app/content/backoffice/backoffice-routing.module.ts
+++ src/app/content/backoffice/backoffice-routing.module.ts
@@ -1,30 +1,42 @@
 import { DashboardComponent } from './content/dashboard/dashboard.component';
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
 import { UserSettingsComponent } from './content/user-settings/user-settings.component';
 import { BackofficeComponent } from './backoffice.component';
+import { ChartsComponent } from './content/charts/charts.component';
+import { EventsComponent } from './content/events/events.component';
 
 const routes: Routes = [
-  {
-    path: '',
-    component: BackofficeComponent,
-    children: [
-      {
+    {
         path: '',
-        component: DashboardComponent,
-        data: { state: 'dashboard' }
-      },
-      {
-        path: 'user',
-        component: UserSettingsComponent,
-        data: { state: 'user' }
-      }
-    ]
-  },
+        component: BackofficeComponent,
+        children: [
+            {
+                path: '',
+                component: DashboardComponent,
+                data: { state: 'dashboard' },
+            },
+            {
+                path: 'user',
+                component: UserSettingsComponent,
+                data: { state: 'user' },
+            },
+            {
+                path: 'charts',
+                component: ChartsComponent,
+                data: { state: 'charts' },
+            },
+            {
+                path: 'events',
+                component: EventsComponent,
+                data: { state: 'events' },
+            },
+        ],
+    },
 ];
 
 @NgModule({
-  imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
+    imports: [RouterModule.forChild(routes)],
+    exports: [RouterModule],
 })
-export class BackoffficeRoutingModule { }
+export class BackoffficeRoutingModule {}
