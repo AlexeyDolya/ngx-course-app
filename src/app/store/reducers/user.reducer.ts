47cb22d816ebed3a85df981dbd1efc9db8241dbb
thegrep01 <den.belotserkovets@gmail.com>
2019-04-24 16:09:25 +0300

    Fix

--- src/app/store/reducers/user.reducer.ts
+++ src/app/store/reducers/user.reducer.ts
@@ -1,4 +1,11 @@
-import { SET_USER } from '../actions/user.action';
+import { EDIT_USER_SUCCESS, SET_USER } from '../actions/user.action';
+
+export type IAdress = {
+    street: string;
+    city: string;
+    state: string;
+    zip: string;
+};
 
 export interface IUser {
     name: string;
@@ -8,16 +15,18 @@ export interface IUser {
     email: string;
     username: string;
     _id: string;
+    adress?: IAdress[];
 }
 
 export const initialState: IUser = {
-    name: 'qwe',
-    surname: 'qwe',
+    name: '',
+    surname: '',
     username: '',
     email: '',
     accessToken: '',
     createdAt: new Date(),
     _id: '',
+    adress: [],
 };
 
 // tslint:disable-next-line: no-any
@@ -29,7 +38,12 @@ export function userReducer(state: IUser = initialState, action: any): IUser {
                 ...action.payload,
             };
         }
-
+        case EDIT_USER_SUCCESS: {
+            return {
+                ...state,
+                ...action.payload,
+            };
+        }
         default: {
             return state;
         }
